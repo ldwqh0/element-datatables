97f0cc75e5ae2732ca202f9ddfb6bedf94c60d93
ldwqh0 <ldwqh0@outlook.com>
2018-03-26 18:47:24 +0800

    初始化测试代码

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 console.log('Created By ldwqh0@outlook.com')
