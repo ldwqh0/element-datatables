3cbd15d5a21778917e6219cb2acaf753363519b1
LiDong <ldwqh0@outlook.com>
2020-01-12 18:02:11 +0800

    修改编译脚本

--- src/components/config.js
+++ src/components/config.js
@@ -1,15 +1,5 @@
-'use strict'
+import axios from 'axios'
 
-Object.defineProperty(exports, '__esModule', {
-  value: true
-})
-
-var _axios = require('axios')
-
-var _axios2 = _interopRequireDefault(_axios)
-
-function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
-
-exports.default = {
-  $http: _axios2.default.create()
+export default {
+  $http: axios.create()
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,28 +1,9 @@
-'use strict'
+import EleDataTables from './EleDataTables'
+import config from './config'
+import axios from 'axios'
 
-Object.defineProperty(exports, '__esModule', {
-  value: true
-})
-
-var _EleDataTables = require('./EleDataTables')
-
-var _EleDataTables2 = _interopRequireDefault(_EleDataTables)
-
-var _config = require('./config')
-
-var _config2 = _interopRequireDefault(_config)
-
-var _axios = require('axios')
-
-var _axios2 = _interopRequireDefault(_axios)
-
-function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
-
-_EleDataTables2.default.install = function (Vue, _ref) {
-  var _ref$httpInstance = _ref.httpInstance,
-    httpInstance = _ref$httpInstance === undefined ? _axios2.default.create() : _ref$httpInstance
-
-  _config2.default.$http = httpInstance
-  Vue.component('EleDataTables', _EleDataTables2.default)
+EleDataTables.install = function (Vue, { httpInstance = axios.create() } = {}) {
+  config.$http = httpInstance
+  Vue.component('EleDataTables', EleDataTables)
 }
-exports.default = _EleDataTables2.default
+export default EleDataTables
