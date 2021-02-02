df89b426ba100ac0de8537da292c16630d71d5db
ldwqh0 <ldwqh0@outlook.com>
2021-02-02 11:23:12 +0800

    修改表格排序不正常的bug

--- src/lib/ele-data-tables/EleDataTables.vue
+++ src/lib/ele-data-tables/EleDataTables.vue
@@ -6,7 +6,7 @@
              :server-params="serverParams"
              :save-state="saveState"
              :debounce-time="debounceTime"
-             :sort="sort"
+             :sort="sortToUse"
              :http="http">
     <template #list="{data}">
       <slot name="table" :data="data">
@@ -40,7 +40,7 @@
 
   interface Sort {
     prop: string,
-    order: string
+    order?: 'ascending' | 'asc' | 'descending' | 'desc' | null
   }
 
   interface Pagination {
@@ -67,64 +67,98 @@
         required: false,
         default: (): null => null,
         type: [Array] as PropType<any[] | null>
-    },
-    ajax: {
-      required: false,
-      default: (): null => null,
-      type: [String, Object, Function] as PropType<string | AjaxFunction | AxiosRequestConfig | null>
-    },
-    serverParams: {
-      required: false,
-      default: () => ({}),
-      type: [Object] as PropType<object>
-    },
-    /**
-     * 一个axios的实例
-     */
-    http: {
-      type: [Function] as PropType<AxiosInstance | null>,
-      required: false,
-      default: null
-    },
-    saveState: { // 是否保存表格状态
-      required: false,
-      default: () => false,
-      type: [Boolean, String]
-    },
-    debounceTime: {
-      required: false,
-      type: [Number],
-      default: () => 500
-    }
-  },
-  methods: {
-    reloadData () {
-      (this.$refs.list as any).reloadData()
-    },
-    selectionChange (selection: any) {
-      this.$emit('selection-change', selection)
-    },
-    cellClick (row: any, column: any, cell: any, event: any) {
-      this.$emit('cell-click', row, column, cell, event)
-    },
-    rowClick (row: any, column: any, event: any) {
-      this.$emit("row-click", row, column, event)
-    },
-    sortChange (event:any) {
-      if (this.data && this.data.length > 0) {
-        // 如果是本地数据，不进行特殊处理
-      } else {
-        this.sort = event
-        // 如果是服务端数据，重新加载数据
-        this.reloadData()
+      },
+      ajax: {
+        required: false,
+        default: (): null => null,
+        type: [String, Object, Function] as PropType<string | AjaxFunction | AxiosRequestConfig | null>
+      },
+      serverParams: {
+        required: false,
+        default: () => ({}),
+        type: [Object] as PropType<object>
+      },
+      /**
+       * 一个axios的实例
+       */
+      http: {
+        type: [Function] as PropType<AxiosInstance | null>,
+        required: false,
+        default: null
+      },
+      saveState: { // 是否保存表格状态
+        required: false,
+        default: () => false,
+        type: [Boolean, String]
+      },
+      debounceTime: {
+        required: false,
+        type: [Number],
+        default: () => 500
+      },
+      sort: {
+        required: false,
+        default: () => ({}),
+        type: [Object, Array] as PropType<Sort | Sort[]>
       }
-      this.$emit('sort-change', event)
+    },
+    methods: {
+      reloadData () {
+        (this.$refs.list as any).reloadData()
+      },
+      selectionChange (selection: any) {
+        this.$emit('selection-change', selection)
+      },
+      cellClick (row: any, column: any, cell: any, event: any) {
+        this.$emit('cell-click', row, column, cell, event)
+      },
+      rowClick (row: any, column: any, event: any) {
+        this.$emit('row-click', row, column, event)
+      },
+      // 重新配置排序字段
+      resetSort ({ prop, order }: Sort) {
+        const extIdx = this.sorts.findIndex(s => s.prop === prop)
+        if (order === undefined || order === null) {
+          if (extIdx > -1) {
+            this.sorts.splice(extIdx, 1)
+          }
+        } else {
+          if (extIdx > -1) {
+            this.sorts[extIdx].order = order
+          } else {
+            this.sorts.push({ prop, order })
+          }
+        }
+      },
+      sortChange (event: any) {
+
+        if (this.data && this.data.length > 0) {
+          // 如果是本地数据，不进行特殊处理
+        } else {
+          this.resetSort(event)
+          // 如果是服务端数据，重新加载数据
+          this.reloadData()
+        }
+        this.$emit('sort-change', event)
+      }
+    },
+    computed: {
+      sortToUse (): Sort[] {
+        const result: Sort[] = []
+        if (Array.isArray(this.sort)) {
+          result.push(...this.sort)
+        } else {
+          result.push(this.sort as Sort)
+        }
+        result.push(...this.sorts)
+        return result
+      }
+    },
+    data () {
+      const data: { sorts: Sort[] } = {
+        sorts: []
+      }
+      return data
     }
-  },
-  data () {
-    return {
-      sort: {}
-    }
-  }
-})
+  })
 </script>
